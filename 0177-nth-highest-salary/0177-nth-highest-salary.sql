353d2f496d808e097de178b6b05de232f0bb1026
Ayaan <109950265+kimkyeongdong@users.noreply.github.com>
2023-01-25 08:39:53 +0900

    Time: 667 ms (29.14%), Space: 0B (100.00%) - LeetHub

--- 0177-nth-highest-salary/0177-nth-highest-salary.sql
+++ 0177-nth-highest-salary/0177-nth-highest-salary.sql
@@ -1,9 +1,9 @@
 CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
 BEGIN
-SET N = N-1;
+SET N = N-1; #변수 설정
   RETURN (
       SELECT DISTINCT(salary) from Employee order by salary DESC
-      LIMIT 1 OFFSET N
+      LIMIT 1 OFFSET N # Limit 행갯수, offset 시작행
       
   );
 END
